add6c109c0899ef139f9796a09272a6e4335d863
Christian Augusto Martins dos Santos Barros <christianbetta@gmail.com>
2022-07-12 17:41:44 -0300

    feat: adding const ARRAY_SIZE_INCREMENTOR

--- golang/main.go
+++ golang/main.go
@@ -10,7 +10,7 @@ import (
 )
 
 func main() {
-	for size := constants.ARRAY_MIN_SIZE; size <= constants.ARRAY_MAX_SIZE; size++ {
+	for size := constants.ARRAY_MIN_SIZE; size <= constants.ARRAY_MAX_SIZE; size += constants.ARRAY_SIZE_INCREMENTOR {
 		fmt.Printf("size: %v\n", size)
 		sort(size)
 		separator()
