33c1b32730163eb17e3086d77037e2566a199b2b
Christian Augusto Martins dos Santos Barros <christianbetta@gmail.com>
2022-07-18 18:24:00 -0300

    javascript: DEBUG option

--- javascript/constants/index.js
+++ javascript/constants/index.js
@@ -1,16 +1,15 @@
-const LOGS = true;
-const TOTAL_LOGS = true;
+const DEBUG = true;
 
-const ARRAY_SIZES = [8, 16];
-// const ARRAY_SIZES = [
-//   8, 16, 100, 1_000, 10_000, 25_000, 50_000, 75_000, 100_000, 250_000, 500_000, 750_000, 1_000_000, 5_000_000,
-// ];
+const LOGS = true;
+const TOTAL_LOGS = DEBUG;
+
+const ARRAY_SIZES = DEBUG
+  ? [8, 16]
+  : [8, 16, 100, 1_000, 10_000, 25_000, 50_000, 75_000, 100_000, 250_000, 500_000, 750_000, 1_000_000, 5_000_000];
 const THREADS_NUMBER = 4;
 
 const MIN_NUMBER = 0;
-const MAX_NUMBER = 16;
-// const MIN_NUMBER = 0;
-// const MAX_NUMBER = 20_000_000_00;
+const MAX_NUMBER = DEBUG ? 16 : 20_000_000_00;
 
 module.exports = {
   LOGS,
