1079e8655ff135688afeb69e4d23559f44716cbe
Christian Augusto Martins dos Santos Barros <christianbetta@gmail.com>
2022-08-08 22:48:03 -0300

    run.cmd

---
+++ golang/run.cmd
@@ -0,0 +1,1 @@
+del /S main.exe && go build main.go && go run main.go > log.log
---
+++ run.cmd
@@ -0,0 +1,5 @@
+echo running golang... && cd golang && run.cmd && cd .. &&
+  echo running java... && cd java && run.cmd && cd .. &&
+  echo running javascript... && cd javascript && run.cmd && cd .. &&
+  echo running python... && cd python && run.cmd
+
