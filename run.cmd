ec36ae0c81fbd6d24b424541469e60fe9e255046
Christian Augusto Martins dos Santos Barros <christianbetta@gmail.com>
2022-09-13 14:33:22 -0300

    adding c++ to run.cmd

--- run.cmd
+++ run.cmd
@@ -2,4 +2,5 @@ echo running golang... && cd golang && run.cmd && cd .. && ^
 echo running java... && cd java && run.cmd && cd .. && ^
 echo running javascript... && cd javascript && run.cmd && cd .. && ^
 echo running python... && cd python && run.cmd && cd ..
+echo running cpp... && cd cpp && run.cmd && cd ..
 
